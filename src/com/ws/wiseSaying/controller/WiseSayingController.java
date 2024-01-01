ef01d85d0ece24b0deff403f8e4ff5f12a96c11e
ohyo555 <555gywn@gmail.com>
2024-01-01 17:21:55 +0900

    14단계, 명언 수정 기능 구현

--- src/com/ws/wiseSaying/controller/WiseSayingController.java
+++ src/com/ws/wiseSaying/controller/WiseSayingController.java
@@ -80,4 +80,34 @@ public class WiseSayingController {
 		return null;
 	}
 
+	public void modify(Rq rq) {
+		int id = rq.getIntParam("id", -1);
+
+		if (id == -1) {
+			System.out.println("id(정수)를 제대로 입력해주세요");
+			return;
+		}
+		// 입력된 id와 일치하는 명언 객체 찾기
+		WiseSaying wiseSaying = findById(id);
+
+		if (wiseSaying == null) {
+			System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
+			return;
+		}
+
+		// 찾은 명언 객체를 object기반으로 수정
+		System.out.println("명언(기존) :" + wiseSaying.getContent());
+		System.out.println("작가(기존) :" + wiseSaying.getAuthor());
+
+		System.out.print("명언 : ");
+		String content = Container.getScanner().nextLine().trim();
+		System.out.print("작가 : ");
+		String author = Container.getScanner().nextLine().trim();
+
+		wiseSaying.setContent(content);
+		wiseSaying.setAuthor(author);
+
+		System.out.printf("%d번 명언이 수정되었습니다.\n", id);
+	}
+
 }
